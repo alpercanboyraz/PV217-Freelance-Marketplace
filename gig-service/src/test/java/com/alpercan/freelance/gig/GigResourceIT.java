f24d1b01067d565eb3e7342efc1ea0a5f911d4b0
alpercanboyraz <alpercanboyraz@student-17-32.eduroam.muni.cz>
2025-10-28 23:04:08 +0100

    feat(gig-service)

---
+++ gig-service/src/test/java/com/alpercan/freelance/gig/GigResourceIT.java
@@ -0,0 +1,8 @@
+package com.alpercan.freelance.gig;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GigResourceIT extends GigResourceTest {
+    // Execute the same tests but in packaged mode.
+}
